8ce1e16bc3bf07f8a43a1899f2a4726c3a4d3387
rahulra17 <rahulramineni@utexas.edu>
2025-07-27 12:51:08 -0500

    put everything together in main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from utils.js_resume_parser import parse_job_description, parse_resume
-from llm.ChatBedrock import ClaudeSonnet
-from analysis.style_extractor import extract_style_summary 
+from llm.ChatBedrock import invoke_claude, ClaudeSonnet
+from analysis.style_extractor import extract_full_style
+from ingestion.doc_ingestor import load_documents, chunk_documents
 
+resume_txt = "" 
+with open("data/resume.txt", "r", encoding="utf-8") as f:
+    resume_text = f.read()
 
+jd_text = ""
+with open("data/job_description.txt", "r", encoding="utf-8") as f:
+    jd_text = f.read()
+
+resume = parse_resume(resume_txt, ClaudeSonnet)
+
+jd = parse_job_description(jd_text, ClaudeSonnet)
+
+docs = load_documents("./data/samples/")
+
+chunks = chunk_documents(docs)
+
+style_profile = extract_full_style(chunks, ClaudeSonnet)
+
+final_result = invoke_claude(style_profile, resume, jd)
+
+print(final_result)
